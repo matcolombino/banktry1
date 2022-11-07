b1e3b5a469476dd3407005d1212366b02353de65
matcolombino <syde4k@gmail.com>
2022-11-07 12:42:27 -0300

    BankTestAlpha

---
+++ BankTest/Account.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace BankTest
+{
+    internal class Account
+    {
+        public string _numero;
+        public string Nome { get; private set; }       
+        
+        public double Saldo { get; private set; }
+
+        public Account(string nome, double saldoinicial)
+        {           
+            Nome = nome;
+            Saldo = saldoinicial;
+        }
+           
+         
+
+        public string Numero { 
+            get { return _numero; }
+            set { if (value.Length == 4)
+                {
+                    _numero = value;
+                }
+            else { _numero = "Conta não encontrada"; }
+            } 
+        }
+
+       
+
+        public void Deposito(double valor)
+        {
+            Saldo += valor; 
+        }
+
+        public void Saque(double valor)
+        {
+            Saldo -= valor +5;
+        }
+
+        public override string ToString()
+        {
+            return $"Conta: {_numero}, titular: {Nome}, saldo: R${Saldo.ToString("F2")}";
+        }
+
+    }
+}
---
+++ BankTest/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace BankTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Entre o número da conta: ");
+            string numero = Console.ReadLine();
+            Console.WriteLine("Entre o título da conta:");
+            string nome = Console.ReadLine();
+            Console.WriteLine("Hávera depósito inicial?");
+            string resposta = Console.ReadLine();
+
+
+            double valor = 0;
+            if (resposta == "Sim")
+            {   Console.WriteLine("Entre o valor do depósito inicial:");
+                valor = double.Parse(Console.ReadLine());
+            }
+
+            Account x = new Account(nome, valor);
+            x.Numero = numero;
+            Console.WriteLine("Dados da conta:");
+            Console.WriteLine(x);
+
+            Console.WriteLine("Escolha a operação que deseja realizar: ");
+            Console.WriteLine("Digite 1 para realizar um depósito.");
+            Console.WriteLine("Digite 2 para realizar um saque.");
+            Console.WriteLine("Digite 3 para sair.");
+
+            int choice = int.Parse(Console.ReadLine());
+            while (choice <= 3)
+            {
+                
+                if (choice == 1)
+                { Console.WriteLine("Digite a quantia que deseja depositar: ");
+                x.Deposito(double.Parse(Console.ReadLine()));
+                choice = 0;
+                    Console.WriteLine("Dados da conta atualizados:");
+                    Console.WriteLine(x);
+
+                    Console.WriteLine("Deseja realizar alguma outra operação? 1-Depósito| 2-Saque| 3-Sair");
+                    choice = int.Parse(Console.ReadLine());
+                }
+               
+                else if (choice == 2) 
+                    {
+                    Console.WriteLine("Digite a quantia que deseja sacar:");
+                    x.Saque(double.Parse(Console.ReadLine()));
+                    Console.WriteLine("Dados da conta atualizados:");
+                    Console.WriteLine(x);
+
+                    Console.WriteLine("Deseja realizar alguma outra operação? 1-Depósito| 2-Saque| 3-Sair");
+                    choice = int.Parse(Console.ReadLine());
+                }
+
+                
+
+            }
+            Console.WriteLine("Dados da conta atualizados:");
+            Console.WriteLine(x);
+            
+
+
+
+
+
+
+        }
+    }
+}
